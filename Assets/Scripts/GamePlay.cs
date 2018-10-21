57e362397d9fa2cdb3c7a96e4294ad76fc349c3e
Carlos González Mejías <43475579+Zexus412@users.noreply.github.com>
2018-10-21 20:59:00 +0200

    Initial commit

---
+++ Assets/Scripts/GamePlay.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GamePlay : MonoBehaviour {
+
+
+    public MotoControl motorista;
+    public Text recordText;
+    public Button startButton;
+    private int segundosParaEmpezar = 3;
+    private Text mainText;
+    private float tiempoInicial;
+    private float tiempoRecord;
+
+
+	// Use this for initialization
+	void Start () {
+
+        motorista.eliminado += Reiniciar;
+        motorista.finalNivel += Final;
+        motorista.enabled = false;
+        mainText = startButton.GetComponentInChildren<Text>();
+        tiempoRecord = PlayerPrefs.GetFloat("tiempo record", 0);
+        if (tiempoRecord > 0) recordText.text = "Record: " + tiempoRecord.ToString("##.##");
+        recordText.enabled = false;
+		
+	}
+
+    void Reiniciar()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void StartGame()
+    {
+        startButton.enabled = false;
+        mainText.text = "" + segundosParaEmpezar;
+        InvokeRepeating("CuentaAtras", 1, 1);
+    }
+
+    void CuentaAtras()
+    {
+        segundosParaEmpezar--;
+        if (segundosParaEmpezar <= 0)
+        {
+            CancelInvoke();
+            JuegoEmpezado();
+        }
+        else mainText.text = "" + segundosParaEmpezar;
+    }
+
+
+    void JuegoEmpezado()
+    {
+        motorista.enabled = true;
+        tiempoInicial = Time.time;
+        if (tiempoRecord > 0) recordText.enabled = true;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if (motorista.enabled)
+        {
+            mainText.text = "Tiempo: " + (Time.time - tiempoInicial).ToString("##.##");
+        }
+	}
+
+    void Final()
+    {
+        motorista.enabled = false;
+        mainText.text = "Final! " + (Time.time - tiempoInicial);
+        PlayerPrefs.SetFloat("tiempo record", (Time.time
+             - tiempoInicial));
+    }
+}
---
+++ Assets/Scripts/MotoControl.cs
@@ -0,0 +1,174 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MotoControl : MonoBehaviour {
+
+    public float velocidadRotacion = 50;
+    public float velocidadLineal = 1;
+    public Transform ruedaTrasera;
+    public event eliminadoDelegate eliminado;
+    public delegate void eliminadoDelegate();
+
+    private Rigidbody2D rigidbody;
+    private float radioRueda;
+    private List <KeyCode> acciones = new List <KeyCode> ();
+
+    public event triggerDelegate eliminado;
+    public event triggerDelegate finalNivel;
+
+	// Use this for initialization
+	void Start () {
+
+        rigidbody = GetComponent<Rigidbody2D>();
+        radioRueda = GetComponent<CircleCollider2D>().radius + 0.1f;
+	}
+	
+    public void MueveDerecha()
+    {
+        if (TocaElSuelo()) rigidbody.velocity
+
++= new Vector2(transform.right.x * velocidadLineal, transform.right.y * velocidadLineal) * Time.deltaTime;
+        
+    }
+
+    public void MueveIzquierda()
+    {
+        if (TocaElSuelo()) rigidbody.velocity 
+-= new Vector2(transform.right.x * velocidadLineal, transform.right.y * velocidadLineal) * Time.deltaTime;
+
+    }
+
+    public void RotaDerecha()
+    {
+        rigidbody.MoveRotation(rigidbody.rotation - velocidadRotacion * Time.deltaTime);
+    }
+
+    public void RotaIzquierda()
+    {
+        rigidbody.MoveRotation(rigidbody.rotation + velocidadRotacion * Time.deltaTime);
+    }
+
+
+    // Update is called once per frame
+    void Update () {
+
+        ActualizarAccionTeclado(KeyCode.LeftArrow);
+        ActualizarAccionTeclado(KeyCode.RightArrow);
+        ActualizarAccionTeclado(KeyCode.UpArrow);
+        ActualizarAccionTeclado(KeyCode.DownArrow);
+
+        if (acciones.Contains(KeyCode.LeftArrow))
+        {
+            MueveIzquierda();
+        }
+
+
+        if (acciones.Contains(KeyCode.RightArrow))
+        {
+            MueveDerecha();
+        }
+
+
+        if (acciones.Contains(KeyCode.UpArrow))
+        {
+            RotaIzquierda();
+        }
+
+
+        if (acciones.Contains(KeyCode.DownArrow))
+        {
+            RotaDerecha();
+        }
+    }
+
+
+    private void ActualizarAccionTeclado (KeyCode code)
+    {
+        if (Input.GetKeyDown(code))
+        {
+            ActualizarAccionDown(code);
+        }
+
+        if (Input.GetKeyUp(code))
+        {
+            ActualizarAccionUp(code);
+        }
+    }
+    
+    private void ActualizarAccionDown(KeyCode code)
+    {
+        if (!acciones.Contains(code)) acciones.Add(code);
+    }
+
+
+    private void ActualizarAccionUp(KeyCode code)
+    {
+        if (!acciones.Contains(code)) acciones.Remove(code);
+    }
+
+
+    public void MueveDerechaDown()
+    {
+        ActualizarAccionDown(KeyCode.RightArrow);
+    }
+
+    public void MueveIzquierdaDown()
+    {
+        ActualizarAccionDown(KeyCode.LeftArrow);
+    }
+
+    public void RotaIzquierdaDown()
+    {
+        ActualizarAccionDown(KeyCode.UpArrow);
+    }
+
+    public void RotaDerechaDown()
+    {
+        ActualizarAccionDown(KeyCode.DownArrow);
+    }
+
+    public void MueveDerechaUp()
+    {
+        ActualizarAccionUp(KeyCode.RightArrow);
+    }
+
+    public void MueveIzquierdaUp()
+    {
+        ActualizarAccionUp(KeyCode.LeftArrow);
+    }
+
+    public void RotaIzquierdaUp()
+    {
+        ActualizarAccionUp(KeyCode.UpArrow);
+    }
+
+    public void RotaDerechaUp()
+    {
+        ActualizarAccionUp(KeyCode.DownArrow);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag.CompareTo("Finish") != 0)
+        {
+            if (eliminado != null) eliminado();
+
+        }
+        else
+            if (finalNivel != null) finalNivel();
+    }
+
+    bool TocaElSuelo()
+    {
+        if (Physics2D.OverlapCircleAll(ruedaTrasera.position, radioRueda).Length > 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+        
+    }
+}
